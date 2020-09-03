841da50c327d60a7ac1e9c438597e8cbdaf47b28
ankush789 <ankushsharma.4771@gmail.com>
2020-09-03 19:44:05 +0530

    Firebase-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
-
-
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+let firebaseConfig = {
+  apiKey: "AIzaSyAvfk6IBMYLlCf9MFdwpffcz4VDpBTxdog",
+  authDomain: "scribe-4771.firebaseapp.com",
+  databaseURL: "https://scribe-4771.firebaseio.com",
+  projectId: "scribe-4771",
+  storageBucket: "scribe-4771.appspot.com",
+  messagingSenderId: "909760875940",
+  appId: "1:909760875940:web:cfc111df605c548b048531",
+  measurementId: "G-5NVQS9L23E"
+};
+firebase.initializeApp(firebaseConfig);
 @NgModule({
   declarations: [
     AppComponent,
