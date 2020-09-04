9c727715cb1c7b2d7371b4cd4ca77cae3073d719
ankush789 <ankushsharma.4771@gmail.com>
2020-09-04 11:31:35 +0530

    login functionality added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  message: string="";
+  userError:any;
+  constructor(public fb: FormBuilder) {
+    this.form= this.fb.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required]]
+    })
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form){
+    firebase.auth().signInWithEmailAndPassword(form.value.email, form.value.password).then((data)=>{
+      console.log(data);
+      this.message="You have been successfully logged in !!";
+    }).catch((error)=>{
+      console.log(error);
+      this.userError=error;
+    })
   }
 
 }
