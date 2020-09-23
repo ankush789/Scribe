8c49f9226e23d3b26cc7900da6ff1ae6b8520e68
ankush789 <ankushsharma.4771@gmail.com>
2020-09-23 19:36:03 +0530

    Getting Updated Posts from the database after creating a new post

--- src/app/myblogs/myblogs.component.ts
+++ src/app/myblogs/myblogs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore'; 
 
 @Component({
   selector: 'app-myblogs',
@@ -9,24 +10,31 @@ import 'firebase/auth';
 })
 export class MyblogsComponent implements OnInit {
   user:any = {};
- 
+  posts:any[] = [];
   constructor() {
     this.user = firebase.auth().currentUser;
+    this.getPosts();
    }
   
   ngOnInit(): void {
   }
   getPosts(){
     //get the lists of post
-   
+     firebase.firestore().collection("posts").get().then((querySnapshot)=>{
+      console.log(querySnapshot.docs);
+      this.posts = querySnapshot.docs;
+    }).catch((err)=>{
+      console.log(err);
+    })
   }
   onPostCreated(){
     //refresh the list of posts
- 
+        this.posts = [];
+        this.getPosts();
   }
   onDelete(){
     //refresh the list of posts
-  
+
   }
 
 }
