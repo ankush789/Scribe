8fc12d36656ef0cc22425b4b5b656cf7ebf82051
ankush789 <ankushsharma.4771@gmail.com>
2020-09-25 19:27:44 +0530

    Fetching id of the post from the routes and writing view component

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent implements OnInit {
+  post: any = {};
+  constructor(public activateRoute: ActivatedRoute, public ngZone: NgZone) { 
+    let postId = this.activateRoute.snapshot.paramMap.get("postId");
 
-  constructor() { }
+    firebase.firestore().collection("posts").doc(postId).get().then(( docSnapshot )=>{
+      this.ngZone.run(()=>{
+        this.post = docSnapshot.data();
+        console.log(this.post);
+      })
+    })
+  }
 
   ngOnInit(): void {
   }
